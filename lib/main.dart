14590d7a1e40a6c7cd3f39e6282c7e409821671b
Pierre Junior <pierresangwa@gmail.com>
2026-01-23 23:30:56 +0200

    removed import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/lang/locale_keys.g.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
